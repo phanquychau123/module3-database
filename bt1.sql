8c17cdbf33f6e6907c618d553b8e5996a45944b0
phanquychau123 <141648218+phanquychau123@users.noreply.github.com>
2023-12-06 18:56:45 +0900

    Add files via upload

---
+++ bt1.sql
@@ -0,0 +1,11 @@
+create database studentmanagement;
+create table class(
+id bigint,
+name varchar(255)
+);
+create table teacher(
+id bigint,
+name varchar(255),
+age tinyint,
+country varchar(255)
+);
